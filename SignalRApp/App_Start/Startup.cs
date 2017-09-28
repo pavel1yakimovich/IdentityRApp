742f7bb11928c18613ada713f75780223becf39d
pavel1yakimovich <pavel1yakimovich@gmail.com>
2017-09-28 22:59:01 +0300

    chat via SignalR added

---
+++ SignalRApp/App_Start/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalRApp.App_Start.Startup))]
+
+namespace SignalRApp.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
---
+++ SignalRApp/Hubs/ChatHub.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using SignalRApp.Models;
+
+namespace SignalRApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private static List<User> users = new List<User>();
+       
+        public void Send (string name, string msg)
+        {
+            Clients.All.addMessage(name, msg);
+        }
+
+        public void Connect (string userName)
+        {
+            var id = Context.ConnectionId;
+
+            if (!users.Any(u => u.ConnectionId == id))
+            {
+                users.Add(new User { ConnectionId = id, UserName = userName });
+
+                Clients.Caller.onConnected(id, userName, users);
+
+                Clients.AllExcept(id).onNewUserConnected(id, userName);
+            }
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            var connectionId = Context.ConnectionId;
+            var item = users.FirstOrDefault(u => u.ConnectionId == connectionId);
+
+            if (item != null)
+            {
+                users.Remove(item);
+                Clients.All.onUserDisconnected(connectionId, item.UserName);
+            }
+
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
